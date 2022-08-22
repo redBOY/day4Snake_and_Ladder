1e2cbe1a0b31b7a7cd805827b9c477cbf16de6e0
“Adithya <adithyasrinivas66@gmail.com>
2022-08-22 11:35:06 +0530

    Welcome to Snake and ladder

---
+++ Snake_And_Ladder.java
@@ -0,0 +1,10 @@
+package main.main.day4Snake_And_Ladder;
+
+public class Snake_And_Ladder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake Ladder Game");
+    }
+
+
+}
+
