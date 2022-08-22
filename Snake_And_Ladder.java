3e87e448072f1ef38de195a148c6f6cbd57fda93
“Adithya <adithyasrinivas66@gmail.com>
2022-08-22 12:04:53 +0530

    uc1 commit

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -1,10 +1,12 @@
 package main.main.day4Snake_And_Ladder;
 
 public class Snake_And_Ladder {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Snake Ladder Game");
+    
+        static final int START_POSITION = 0;
+
+        public static void main(String[] args) {
+
+            System.out.println("Welcome to Snake and ladder Game");
+        }
     }
-
-
-}
 
