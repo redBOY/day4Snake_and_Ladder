88ffd8da594caf9eac5a64f4a3660903c5f5e28e
“Adithya <adithyasrinivas66@gmail.com>
2022-08-22 12:31:21 +0530

    uc4 commit

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -1,12 +1,39 @@
 package main.main.day4Snake_And_Ladder;
 
 public class Snake_And_Ladder {
-    
-        static final int START_POSITION = 0;
 
-        public static void main(String[] args) {
+    static final int START_POSITION = 0;
+    static final int NO_PLAY = 0;
+    static final int IF_LADDER = 1;
+    static final int IF_SNAKE = 2;
+    static int currentposition = 0;
 
-            System.out.println("Welcome to Snake and ladder Game");
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and ladder game");
+        while(currentposition<100) {
+            int dice = (int) (Math.random() * 10) % 6 + 1;
+            int option = (int) (Math.random() * 10) % 3;
+
+            if (option == IF_LADDER) {
+                System.out.println("ladder!!");
+                currentposition +=dice;
+
+            } else if (option == IF_SNAKE) {
+                System.out.println("snake!!");
+                currentposition -=dice;
+
+
+            } else {
+                System.out.println("N0 play");
+                currentposition=currentposition;
+            }
+            if(currentposition<START_POSITION){
+                currentposition=START_POSITION;
+            }
+
+            System.out.println("currentposition: "+currentposition);
         }
-    }
 
+
+    }
+}
