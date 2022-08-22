bf60f08ae2f8b23b4f9193bb1ac92844188cd3c6
“Adithya <adithyasrinivas66@gmail.com>
2022-08-22 12:20:45 +0530

    uc2 commit

--- Snake_And_Ladder.java
+++ Snake_And_Ladder.java
@@ -7,6 +7,15 @@ public class Snake_And_Ladder {
         public static void main(String[] args) {
 
             System.out.println("Welcome to Snake and ladder Game");
-        }
-    }
+
+                    int dice = (int) (Math.random()*10)%6+1;
+                    System.out.println("Dice rolled value: "+dice);
+
+                }
+
+
+
+
+            }
+    
 
